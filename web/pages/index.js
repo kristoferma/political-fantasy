d79da01087e5cc0376f05bbcdddc3ce901647662
Kristófer Másson <kristoferma@me.com>
2019-02-25 15:51:40 -0500

    Create signup api

---
+++ web/pages/index.js
@@ -0,0 +1,12 @@
+import { Layout } from 'antd'
+
+function Home() {
+  return (
+    <Layout>
+      <Layout.Header>Header</Layout.Header>
+      <Layout.Content />
+    </Layout>
+  )
+}
+
+export default Home
